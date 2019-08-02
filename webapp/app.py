25249c72d2f7ca8df8c4d736d4b1071cbeec8eeb
Joseph Dane <Joseph_Dane@symantec.com>
2019-08-01 20:17:29 -0400

    Updated challenges page structure, added level 6, removed password in code and added creds file

--- webapp/app.py
+++ webapp/app.py
@@ -27,12 +27,16 @@ def level4():
 def level5():
   return render_template('level5.html')
 
+@app.route('/level6')
+def level6():
+  return render_template('level6.html')
+
+
 @app.route('/login1', methods = ['GET'])
 def login1():
   username = request.args.get('username')
   password = request.args.get('password')
 
-  # foo - wordpass
   correct_creds = ['acbd18db4cc2f85cedef654fccc4a4d8', 'e80eded141e1295d694cd35cf2b8f675']
   username_hash = hashlib.md5(username.encode()).hexdigest()
   pw_hash = hashlib.md5(password.encode()).hexdigest()
@@ -47,7 +51,6 @@ def login2():
   username = request.form.get('username')
   password = request.form.get('password')
 
-  # kar - willow
   correct_creds = ['aa8ae3b340c34010e4500a0d6294dc2c', '592cec0a3fc4d8cf9b6e57a09bff554b']
   username_hash = hashlib.md5(username.encode()).hexdigest()
   pw_hash = hashlib.md5(password.encode()).hexdigest()
@@ -62,7 +65,6 @@ def login3():
   auth_header = request.headers.get('Authorization')
   if auth_header:
     try:
-      # wre - trout
       correct_creds = ['80743387e7cd5fb5cc71693b16859e80', '14bb02c03c92af99ac2cb42dea8a1bd2']
       username, password = auth_header.split(':')
       username_hash = hashlib.md5(username.encode()).hexdigest()
